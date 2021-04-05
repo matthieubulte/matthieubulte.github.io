b278df15f88d4d7ca10980dbfb422f7e392d2341
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2021-04-05 13:30:11 +0200

    Add steph stuff

---
+++ stephsteph/sliding.js
@@ -0,0 +1,201 @@
+// Begin game once DOM loaded
+document.addEventListener("DOMContentLoaded", game);
+
+function game() {
+
+  // Data structure to hold positions of tiles
+  var parentX = document.querySelector(".sliding-puzzle").clientHeight;
+  var baseDistance = 31;
+  var tileMap = {
+    1: {
+      tileNumber: 1,
+      position: 1,
+      top: 0,
+      left: 0
+    },
+    2: {
+      tileNumber: 2,
+      position: 2,
+      top: 0,
+      left: baseDistance * 1
+    },
+    3: {
+      tileNumber: 3,
+      position: 3,
+      top: 0,
+      left: baseDistance * 2
+    },
+    4: {
+      tileNumber: 4,
+      position: 4,
+      top: baseDistance,
+      left: 0
+    },
+    5: {
+      tileNumber: 5,
+      position: 5,
+      top: baseDistance,
+      left: baseDistance
+    },
+    6: {
+      tileNumber: 6,
+      position: 6,
+      top: baseDistance,
+      left: baseDistance * 2
+    },
+    7: {
+      tileNumber: 7,
+      position: 7,
+      top: baseDistance * 2,
+      left: 0
+    },
+    8: {
+      tileNumber: 8,
+      position: 8,
+      top: baseDistance * 2,
+      left: baseDistance
+    },
+    empty: {
+      position: 9,
+      top: baseDistance * 2,
+      left: baseDistance * 2
+    }
+  }
+
+  // Movement map
+  function movementMap(position) {
+    if (position == 9) return [6, 8];
+    if (position == 8) return [5, 7, 9];
+    if (position == 7) return [4, 8];
+    if (position == 6) return [3, 5, 9];
+    if (position == 5) return [2, 4, 6, 8];
+    if (position == 4) return [1, 5, 7];
+    if (position == 3) return [2, 6];
+    if (position == 2) return [1, 3, 5];
+    if (position == 1) return [2, 4];
+  }
+
+  // Board setup according to the tileMap
+  var tiles = document.querySelectorAll('.tile');
+  var delay = -50;
+  for(var i = 0; i < tiles.length; i++) {
+    tiles[i].addEventListener('click', tileClicked ,true );
+    delay += 50;
+    setTimeout(setup, delay, tiles[i]);
+  }
+
+  function setup(tile) {
+    var tileId = tile.id;
+    var xMovement = parentX * (tileMap[tileId].left/100);
+    var yMovement = parentX * (tileMap[tileId].top/100);
+    var translateString = "translateX(" + xMovement + "px) " + "translateY(" + yMovement + "px)"
+    tile.style.webkitTransform = translateString;
+  }
+
+  function tileClicked(event) {
+    var target = event.target.id ?
+      event.target : event.target.parentElement;
+    
+    moveTile(target);
+    if (checkSolution()) {
+
+      setTimeout(function() {
+        document.querySelector(".sliding-puzzle").innerHTML = '<img src="sliding_imgs/final.jpg" style="width:100%; height:100%;"/>';
+
+        alert("Yippie!");
+      }, 1000);
+      
+      
+      console.log("You win!");
+    }
+  }
+
+  // Moves tile to empty spot
+  // Returns error message if tile cannot be moved
+  function moveTile(tile) {
+    // Check if Tile can be moved 
+    // (must be touching empty tile)
+    // (must be directly perpendicular to empty tile)
+    var tileNumber = tile.id || tile;
+    if (!tileMovable(tileNumber)) {
+      return;
+    }
+
+    // Swap tile with empty tile
+    var emptyTop = tileMap.empty.top;
+    var emptyLeft = tileMap.empty.left;
+    var emptyPosition = tileMap.empty.position;
+    tileMap.empty.top = tileMap[tileNumber].top;
+    tileMap.empty.left = tileMap[tileNumber].left;
+    tileMap.empty.position = tileMap[tileNumber].position;
+
+    // tile.style.top = emptyTop  + '%'; 
+    // tile.style.left = emptyLeft  + '%';
+
+    var xMovement = parentX * (emptyLeft/100);
+    var yMovement = parentX * (emptyTop/100);
+    var translateString = "translateX(" + xMovement + "px) " + "translateY(" + yMovement + "px)"
+    tile.style.webkitTransform = translateString;
+
+    tileMap[tileNumber].top = emptyTop;
+    tileMap[tileNumber].left = emptyLeft;
+    tileMap[tileNumber].position = emptyPosition;
+  }
+
+
+  // Determines whether a given tile can be moved
+  function tileMovable(tileNumber) {
+    var selectedTile = tileMap[tileNumber];
+    var emptyTile = tileMap.empty;
+    var movableTiles = movementMap(emptyTile.position);
+
+    if (movableTiles.includes(selectedTile.position)) {
+      return true;
+    } else {
+      return false;
+    }
+
+
+
+  }
+
+  // Returns true/false based on if the puzzle has been solved
+  function checkSolution() {
+    if (tileMap.empty.position !== 9) return false;
+
+    for (var key in tileMap) {
+      if ((key != 1) && (key != "empty")) {
+        if (tileMap[key].position < tileMap[key-1].position) return false;
+      }
+    }
+
+    return true;
+  }
+
+
+
+  var lastShuffled;
+  function shuffleLoop() {
+    var emptyPosition = tileMap.empty.position;
+    var shuffleTiles = movementMap(emptyPosition);
+    var tilePosition = shuffleTiles[Math.floor(Math.floor(Math.random()*shuffleTiles.length))];
+    var locatedTile;
+    for(var i = 1; i <= 8; i++) {
+      if (tileMap[i].position == tilePosition) {
+        var locatedTileNumber = tileMap[i].tileNumber;
+        locatedTile = tiles[locatedTileNumber-1];
+      }
+    }
+    if (lastShuffled != locatedTileNumber) {
+      moveTile(locatedTile);
+      lastShuffled = locatedTileNumber;
+    } else {
+      shuffleLoop();
+    }
+  
+  }
+  // for(var i = 0; i < 20; i++) {
+  //   shuffleLoop();
+  // }
+}
+
